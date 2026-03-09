c58cf3c87fad6e8acdf1d457548f4d917eed216a
Michael Buluma <buluma@users.noreply.github.com>
2026-03-09 10:08:51 +0300

    Update style.ts

--- src/template/style.ts
+++ src/template/style.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from "fs";
 
 export const importCss = () => `${readFileSync(
-  "node_modules/tailwindcss/index.css"
+  "node_modules/tailwindcss/dist/tailwind.min.css"
 ).toString()}
 ${readFileSync("node_modules/react-vis/dist/style.css").toString()}`;
