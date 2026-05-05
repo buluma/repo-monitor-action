3daaa42fab2eacbd225d0c1362a5950a5a504899
Michael Buluma <1452922+buluma@users.noreply.github.com>
2026-05-05 07:30:15 +0300

    Update style.ts

--- src/template/style.ts
+++ src/template/style.ts
@@ -1,6 +1,7 @@
 import { readFileSync } from "fs";
 
-export const importCss = () => `${readFileSync(
-  "node_modules/tailwindcss/dist/tailwind.min.css"
-).toString()}
-${readFileSync("node_modules/react-vis/dist/style.css").toString()}`;
+const readCssFromPackage = (id: string): string =>
+  readFileSync(require.resolve(id), "utf8");
+
+export const importCss = () => `${readCssFromPackage("tailwindcss/index.css")}
+${readCssFromPackage("react-vis/dist/style.css")}`;
